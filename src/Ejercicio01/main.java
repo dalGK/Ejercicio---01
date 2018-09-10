c9f833ad8be9d68f65fbf00f26c85f5a0c8c31b8
Dal <Dalvin20099@gmail.com>
2018-09-10 08:39:49 +0200

    Initial commit

---
+++ src/Ejercicio01/main.java
@@ -0,0 +1,13 @@
+package Ejercicio01;
+
+/*
+ * Realizar un programa que escriba “Hola Mundo” (o “hola ke ase”... como prefieras)
+ */
+
+public class main {
+ 
+	public main() {
+		System.out.println("Hola Mundaso");
+	}
+	
+}
